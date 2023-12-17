6df7d1733f3d9bc82971d8fdcf0f00674a7787c9
magura13 <tiagolelis_@hotmail.com>
2023-12-16 22:22:10 -0300

    fix login page

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { LoginService } from '../services/Login';
+import { useLanguage } from '../context/LanguageContext';
 
 function Login() {
+  const { language } = useLanguage();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+
+  const texts = {
+    en: {
+      header: 'Login',
+      emailLabel: 'Email address',
+      passwordLabel: 'Password',
+      signInButton: 'Sign In',
+      invalidCredentials: 'Invalid email/password',
+      errorOccurred: 'An error occurred. Please try again.',
+      loggedInSuccess: 'Logged Successfully! Here is your AccessToken: '
+    },
+    pt: {
+      header: 'Entrar',
+      emailLabel: 'Endereço de email',
+      passwordLabel: 'Senha',
+      signInButton: 'Entrar',
+      invalidCredentials: 'Email/senha inválidos',
+      errorOccurred: 'Ocorreu um erro. Por favor, tente novamente.',
+      loggedInSuccess: 'Login efetuado com sucesso! Aqui está o seu AccessToken: '
+    }
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -19,13 +42,13 @@ function Login() {
       if (response.accessToken) {
         setEmail("");
         setPassword("");
-        alert(`Logged Successfully! Here is your AccessToken: ${response.accessToken}`);
+        alert(texts[language].loggedInSuccess + response.accessToken);
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        setError("Invalid email/password");
+        setError(texts[language].invalidCredentials);
       } else {
-        setError("An error occurred. Please try again.");
+        setError(texts[language].errorOccurred);
       }
     } finally {
       setIsLoading(false);
@@ -33,11 +56,11 @@ function Login() {
   };
 
   return (
-    <div className="container-fluid h-100 d-flex flex-column justify-content-center" style={{ backgroundColor: 'hsl(0, 0%, 96%)', minHeight: '100vh' }}>
-      <div className="row justify-content-center">
+    <div className="container-fluid h-100" style={{ backgroundColor: 'hsl(0, 0%, 96%)', minHeight: '100vh' }}>
+      <div className="row justify-content-center align-items-center h-100">
         <div className="col-lg-6 mb-5 text-center">
           <h1 className="my-5 display-3 fw-bold ls-tight">
-            Login <br />
+            {texts[language].header} <br />
           </h1>
         </div>
         <div className="col-lg-6 mb-5">
@@ -48,20 +71,20 @@ function Login() {
                 <div className="form-outline mb-4">
                   <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" className="form-control" />
                   <label className="form-label" htmlFor="form3Example3">
-                    Email address
+                    {texts[language].emailLabel}
                   </label>
                 </div>
                 <div className="form-outline mb-4">
                   <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="form3Example4" className="form-control" />
                   <label className="form-label" htmlFor="form3Example4">
-                    Password
+                    {texts[language].passwordLabel}
                   </label>
                 </div>
-                <button type="submit"
-                  className="btn btn-block"
-                  style={{ backgroundColor: '#000', color: '#fff' }}>
-                  Sign up
-                </button>
+                <div className="d-flex justify-content-center">
+                  <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#000', color: '#fff' }} disabled={isLoading}>
+                    {texts[language].signInButton}
+                  </button>
+                </div>
               </form>
             </div>
           </div>
