7036e2fd3be3971a8414d96f3075e52ab513a15e
magura13 <tiagolelis_@hotmail.com>
2023-12-16 22:33:13 -0300

    fixing layout

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -56,37 +56,33 @@ function Login() {
   };
 
   return (
-    <div className="container-fluid h-100" style={{ backgroundColor: 'hsl(0, 0%, 96%)', minHeight: '100vh' }}>
-      <div className="row justify-content-center align-items-center h-100">
-        <div className="col-lg-6 mb-5 text-center">
-          <h1 className="my-5 display-3 fw-bold ls-tight">
-            {texts[language].header} <br />
-          </h1>
-        </div>
-        <div className="col-lg-6 mb-5">
-          <div className="card">
-            <div className="card-body py-5 px-md-5">
-              {error && <div className="alert alert-danger" role="alert">{error}</div>}
-              <form onSubmit={handleSubmit}>
-                <div className="form-outline mb-4">
-                  <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" className="form-control" />
-                  <label className="form-label" htmlFor="form3Example3">
-                    {texts[language].emailLabel}
-                  </label>
-                </div>
-                <div className="form-outline mb-4">
-                  <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="form3Example4" className="form-control" />
-                  <label className="form-label" htmlFor="form3Example4">
-                    {texts[language].passwordLabel}
-                  </label>
-                </div>
-                <div className="d-flex justify-content-center">
-                  <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#000', color: '#fff' }} disabled={isLoading}>
-                    {texts[language].signInButton}
-                  </button>
-                </div>
-              </form>
-            </div>
+    <div className="container-fluid h-100 d-flex justify-content-center align-items-center" style={{ backgroundColor: 'hsl(0, 0%, 96%)', minHeight: '100vh' }}>
+      <div className="col-md-6 text-center">
+        <h1 className="my-5 display-3 fw-bold ls-tight">
+          {texts[language].header}
+        </h1>
+        <div className="card">
+          <div className="card-body py-5 px-md-5">
+            {error && <div className="alert alert-danger" role="alert">{error}</div>}
+            <form onSubmit={handleSubmit}>
+            <div className="form-outline mb-4">
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="form3Example3" className="form-control" />
+                <label className="form-label" htmlFor="form3Example3">
+                  {texts[language].emailLabel}
+                </label>
+              </div>
+              <div className="form-outline mb-4">
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="form3Example4" className="form-control" />
+                <label className="form-label" htmlFor="form3Example4">
+                  {texts[language].passwordLabel}
+                </label>
+              </div>
+              <div className="d-flex justify-content-center">
+                <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#000', color: '#fff' }} disabled={isLoading}>
+                  {texts[language].signInButton}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
